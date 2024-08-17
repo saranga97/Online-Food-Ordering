33cdae5f3d1a8bdccb8372f20025079dacec35cb
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-08-17 21:16:05 +0530

    #C004-Order class

---
+++ src/main/java/com/advent/model/Order.java
@@ -0,0 +1,13 @@
+package com.advent.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
