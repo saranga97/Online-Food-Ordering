f0a9dfe8ce7aee6437e8cad1c65096e4ced60895
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-08-18 01:22:26 +0530

    #C007-Added new models

--- src/main/java/com/advent/model/Order.java
+++ src/main/java/com/advent/model/Order.java
@@ -1,13 +1,47 @@
 package com.advent.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @ManyToOne
+    private User customer;
+
+    @ManyToOne
+    @JsonIgnore
+    private Restaurant restaurant;
+
+    private Long totalAmount;
+
+    private String orderStatus;
+
+    private Date createdAt;
+
+    @ManyToOne
+    private Address deliveryAddress;
+
+    @OneToMany
+    private List<Orderitem> items;
+
+    //private Payment payment;
+
+    private int TotalItem;
+
+    private int totalPrice;
+
+
 }
