1e3915332c58f7c77f2d5e0a85f71737f9543195
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-08-30 15:06:43 +0530

    #C013-JWT authentication validation

--- src/main/java/com/advent/config/JwtProvider.java
+++ src/main/java/com/advent/config/JwtProvider.java
@@ -45,6 +45,6 @@ public class JwtProvider {
         for (GrantedAuthority authority: authorities){
             auths.add(authority.getAuthority());
         }
-        return String.join(",",auths)
+        return String.join(",",auths);
     }
 }
