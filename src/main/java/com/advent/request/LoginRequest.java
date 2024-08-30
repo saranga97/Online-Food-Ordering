a7a7d6a17ce3417ffa9f0e348375f918dcc6a9ac
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-08-30 15:26:30 +0530

    #C014-Auth response

---
+++ src/main/java/com/advent/request/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.advent.request;
+
+public class LoginRequest {
+}
---
+++ src/main/java/com/advent/response/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.advent.response;
+
+import com.advent.model.USER_ROLE;
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+    private String jwt;
+
+    private String message;
+
+    private USER_ROLE role;
+}
